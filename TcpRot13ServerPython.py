273ff7211b27aeb122ca1de3841a70dd76556310
Soreasan <soreasan@gmail.com>
2016-12-12 10:15:56 -0700

    Updated TcpRot13ServerPython.py because there were typos.

--- TcpRot13ServerPython.py
+++ TcpRot13ServerPython.py
@@ -6,7 +6,7 @@ import codecs
 
 def rot13_service(c):
     while True:
-        inbytes == c.recv(1024)
+        inbytes = c.recv(1024)
         if inbytes:
             outbytes = codecs.encode(inbytes.decode(), 'rot_13').encode()
             c.send(outbytes)
@@ -19,7 +19,7 @@ s.listen(5)
 print("listening...")
 
 while True:
-    client.addr = s.accept()
+    client, addr = s.accept()
     print("connected...")
     rot13_service(client)
     client.close()
